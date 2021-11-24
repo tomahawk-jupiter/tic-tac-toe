fec597e62f0599c6d36ed354697d73140e0b1c4a
tomahawk-jupiter <brianbrainrogers@gmail.com>
2021-11-24 01:29:57 +0000

    Added reset button

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const playerFactory = (name, sign, moveArray) => {
 
 const display = (() => {
   const gameContainer = document.querySelector('.game-container');
+  const gameFinishedDisplay = document.querySelector('.game-finished-display');
 
   const chooseNames = () => {
     gameContainer.innerHTML = `
-    <form class="name-form">
-      <input id="p1" type="text" name="" placeholder="Who is Noughts?">
-      <input id="p2" type="text" name="" placeholder="Who is Crosses?">
-      <input class="form-btn" type=button name="" value="Start Game">
-    </form>
+      <form class="name-form">
+        <input id="p1" type="text" name="" placeholder="Who is Noughts?">
+        <input id="p2" type="text" name="" placeholder="Who is Crosses?">
+        <input class="form-btn" type=button name="" value="Start Game">
+      </form>
     `;
 
     const formBtn = gameContainer.querySelector('.form-btn');
@@ -37,45 +38,40 @@ const display = (() => {
     });
   }
 
-  const gameWinMessage = (winningThree) => {
+  const gameWinMessage = (winningThree, winner) => {
     winningThree.map(i => {
       console.log(i);
       const winningTile = document.getElementById(String(i));
       winningTile.style.color = 'green';
-
-      
     });
+    gameFinishedDisplay.innerHTML = `<div>${winner} wins the game</div>`;
   }
-
   return {chooseNames, drawBoard, gameWinMessage};
 })();
 
 
-
 const game = (() => {
-  const gameArray = [
+  let gameArray = [
     '.', '.', '.', '.', '.', '.', '.', '.', '.'
   ];
   let turn = 0;
   let p1;
   let p2;
 
-  const winCheck = (playerArray) => {
+  const winCheck = (playerArray, winner) => {
       const winArray = [
         [0,1,2],[3,4,5],[6,7,8],
         [0,3,6],[1,4,7],[2,5,8],
         [0,4,8],[2,4,6]];
 
-        winArray.map(set => {
-          const test = set.every(i => {
-            return playerArray.includes(i);
-          });
-          if (test){
-            display.gameWinMessage(set);
-          }
-          // invoke game won message:
-          //renderDisplay.gameWinMessage(test, firstPlayerTurn);
+      winArray.map(set => {
+        const test = set.every(i => {
+          return playerArray.includes(i);
         });
+        if (test){
+          display.gameWinMessage(set, winner);
+        }
+      });
     }
 
   const startGame = (p1Name, p2Name) => {
@@ -87,108 +83,42 @@ const game = (() => {
   const playerPlaceSign = (e) => {
     const tileIndex = Number(e.target.id);
     let playerArray;
+    let winner;
     if (gameArray[tileIndex] === '.') {
       if (turn % 2 == 0) {
         p1.moveArray.push(tileIndex);
         gameArray.splice(tileIndex, 1, p1.sign);
         playerArray = p1.moveArray;
+        winner = p1.name;
       } else {
         p2.moveArray.push(tileIndex);
         gameArray.splice(tileIndex, 1, p2.sign);
         playerArray = p2.moveArray;
+        winner = p2.name;
       }
       turn++;
       display.drawBoard(gameArray);
-      winCheck(playerArray);
+      winCheck(playerArray, winner);
     }
   }
 
-  return {startGame, playerPlaceSign};
+  const resetGame = () => {
+    gameArray = [
+      '.', '.', '.', '.', '.', '.', '.', '.', '.'
+    ];
+    p1.moveArray = [];
+    p2.moveArray = [];
+    turn = 0;
+  }
+
+  const resetBtn = document.querySelector('.reset-btn');
+  resetBtn.addEventListener('click', ()=> {
+    resetGame();
+    display.chooseNames();
+  });
+
+  return {startGame, playerPlaceSign, resetGame};
 })();
 
+// Starts the game:
 display.chooseNames();
-
-// const playerFactory = (name, symbol, movesArray) => {
-//   return {name, symbol, movesArray};
-// }
-//
-// const renderDisplay = (() => {
-//   const gameContainer = document.querySelector('.game-container');
-//
-//   const drawBoard = (gameArray) => {
-//     gameContainer.replaceChildren();
-//     let index = 0;
-//     gameArray.map(tile => {
-//       let gameTile = document.createElement('div');
-//       gameTile.innerHTML =`<div id=${index} class="game-tile">${tile}</div>`;
-//       gameContainer.appendChild(gameTile);
-//       gameTile.addEventListener('click', game.placeSymbol);
-//       index++;
-//     });
-//   }
-//
-//   const gameWinMessage = (test, firstPlayerTurn) => {
-//     const displayMessage = document.querySelector('.display-message');
-//     if (test) {
-//        const player = firstPlayerTurn ? 'Player One' : 'Player Two';
-//        displayMessage.innerText = `${player} Wins!`;
-//     }
-//   }
-//
-//   return {drawBoard, gameWinMessage};
-// })();
-//
-// const game = (() => {
-//   const gameArray = ['.', '.', '.', '.', '.', '.', '.', '.', '.'];
-//   let firstPlayerTurn = true;
-//
-//   const p1 = playerFactory('p1', 'o', []);
-//   const p2 = playerFactory('p2', 'x', []);
-//
-//   const winCheck = (playerArray) => {
-//       const winArray = [
-//         [0,1,2],[3,4,5],[6,7,8],
-//         [0,3,6],[1,4,7],[2,5,8],
-//         [0,4,8],[2,4,6]];
-//     console.log({playerArray})
-//         winArray.map(set => {
-//           const test = set.every(i => {
-//             return playerArray.includes(i);
-//           });
-//           console.log({test});
-//           // invoke game won message:
-//           renderDisplay.gameWinMessage(test, firstPlayerTurn);
-//         });
-//     }
-//
-//   const placeSymbol = (e) => {
-//     let playerSymbol = firstPlayerTurn ? p1.symbol : p2.symbol;
-//     let index = Number(e.target.id);
-//
-//     const playerArray = firstPlayerTurn ? p1.movesArray : p2.movesArray;
-//     // Don't allow placement on occupied tiles:
-//     if (gameArray[index] === '.') {
-//       if (firstPlayerTurn) {
-//         p1.movesArray.push(index);
-//       } else if (!firstPlayerTurn) {
-//         p2.movesArray.push(index);
-//       }
-//       gameArray.splice(index, 1, playerSymbol);
-//
-//       renderDisplay.drawBoard(gameArray);
-//       winCheck(playerArray);
-//
-//       firstPlayerTurn = firstPlayerTurn ? false : true;
-//     }
-//     // IMPROVE - only start checking on round 3
-//   }
-//
-//   return {placeSymbol, gameArray};
-// })();
-//
-// // start game:
-// renderDisplay.drawBoard(game.gameArray);
-//
-//
-//
-// // renderDisplay.drawBoard(['.', '.', '.', '.', '.', '.', '.', '.', '.']);
